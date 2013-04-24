eb6489b623c934a0e1ed6def6521d39f7ac59cb6
cultofmetatron <cultofmetatron@aumlogic.com>
2013-04-24 13:07:46 -0700

    added in all the files. now to install initialize a remote repo on git

---
+++ package.js
@@ -0,0 +1,16 @@
+/* created by cultofmetatron, blog.peterdecroos.com
+ * Made with love for the meteor community!
+ *
+ */
+Package.describe({
+  summary: "OpenTok Api exposed to meteor"
+});
+
+Npm.depends({"opentok" : "0.3.x"});
+
+Package.on_use(function(api) {
+  api.add_files('client/tokbox-client.js', 'client');
+  api.add_files('server/index.js', 'server');
+});
+
+
